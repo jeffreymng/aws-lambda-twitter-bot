d8c0fc7e0590888567afd802a9590ef3dd6430bc
Jeffrey Ng <jeffrey@machinecdn.com>
2018-06-04 14:48:29 -0500

    add markov chain and tweets

---
+++ index.js
@@ -0,0 +1,54 @@
+const twitter_credentials = require('./twitter-api-credentials');
+const Twitter = require('twitter');
+const MarkovChain = require('markovchain')
+
+let twitter = new Twitter(twitter_credentials);
+
+wordList = "";
+
+exports.tweet = function(event, context) {
+  twitter.get(
+    'statuses/user_timeline',
+    {
+      screen_name: "realDonaldTrump",
+      count: 200,
+      exclude_replies: true,
+      include_rts: false,
+      tweet_mode: 'extended'
+    },
+    function(error, tweets, response) {
+      if (!error) {
+        for (i = 0; i < tweets.length; i++) {
+          wordList += " ";
+          wordList += tweets[i].full_text
+          wordList = wordList.replace(/[, ]+/g, " ").trim()
+          wordList.replace(/http\S+/, '');
+          wordList.replace(/,/, '');
+        }
+        markovChain = new MarkovChain(wordList);
+        jaredTweet(markovChain);
+      }
+    }
+  )
+}
+
+exports.jaredTweet = function(chain) {
+  twitter.post(
+    'statuses/update',
+    {
+      status: chain.start(useUpperCase).end().process()
+    },
+    function(error, tweet, response) {
+      if (error) {
+        jaredTweet()
+      }
+    }
+  )
+}
+
+var useUpperCase = function(wordList) {
+  var tmpList = Object.keys(wordList).filter(function(word) {
+    return word[0] >= 'A' && word[0] <= 'Z'
+  })
+  return tmpList[~~(Math.random()*tmpList.length)]
+}
